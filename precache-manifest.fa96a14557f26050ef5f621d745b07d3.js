8083e9e5e0af899089c960513adfda1d5da5d9ad
wei luo <weiluo0001@gmail.com>
2020-03-04 00:45:36 -0500

    Updates

---
+++ precache-manifest.fa96a14557f26050ef5f621d745b07d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab460949a5440ceda77273607899c8df",
+    "url": "/flu_about/index.html"
+  },
+  {
+    "revision": "9e3b79b9b7225fb11efe",
+    "url": "/flu_about/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1edf52a68b725cd0b5d9",
+    "url": "/flu_about/static/css/main.ba838967.chunk.css"
+  },
+  {
+    "revision": "9e3b79b9b7225fb11efe",
+    "url": "/flu_about/static/js/2.4faf2a4a.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/flu_about/static/js/2.4faf2a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1edf52a68b725cd0b5d9",
+    "url": "/flu_about/static/js/main.e529b66d.chunk.js"
+  },
+  {
+    "revision": "9da88fb3c1c0e7d0acf7",
+    "url": "/flu_about/static/js/runtime-main.6e65f09f.js"
+  },
+  {
+    "revision": "2fc2d10733453f91591e852eb27ad90d",
+    "url": "/flu_about/static/media/ForecastView.2fc2d107.bmp"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flu_about/static/media/logo.5d5d9eef.svg"
+  }
+]);
